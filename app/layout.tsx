3d4e9d9ff974712e1cd2b24f5161241a6bda2032
Isaias Buscarino <isaias.buscarino@hotmail.com>
2023-07-23 21:46:28 +0200

    Config eslint and prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'NextJS Boilerplate',
-  description: 'A simple project starter to work with TypeScript, React, NextJS, Tailwind, Zustand, Jest and Testing library',
-}
+  description:
+    'A simple project starter to work with TypeScript, React, NextJS, Tailwind, Zustand, Jest and Testing library'
+};
 
 export default function RootLayout({
-  children,
+  children
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
